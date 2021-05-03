ff52ee4cb5d461bc0160c3a857437fca4b31829d
Mohamed Aboulmaaly <maboulmaaly@yahoo.com>
2021-05-03 23:54:31 +0000

    route parameters

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ use App\Http\Controllers\ProductsController;
 
 
 Route::get('products', [ProductsController::class, 'index']);
+// Route::get('products/{id}', [ProductsController::class, 'show'])->where('id', '[0-9]+');
+Route::get(
+    'products/{name}/{id}', 
+    [ProductsController::class, 'show']
+)->where([
+    'name' => '[a-zA-Z]+',
+    'id' => '[0-9]+'
+]);
 Route::get('products/about', [ProductsController::class, 'about']);
-
-// Route::get('products/', 'App\Http\Controllers\ProductsController@index');
-
-// Route::get('products/', 'ProductsController@index');
