b7b4d323325df2314714e50a3e922609c6bc7631
Mohamed Aboulmaaly <maboulmaaly@yahoo.com>
2021-05-05 21:38:10 +0000

    database and migrations

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostsController;
 
 Route::get('/', [PagesController::class, 'index']);
 Route::get('/about', [PagesController::class, 'about']);
+
+Route::get('/posts', [PostsController::class, 'index']);
