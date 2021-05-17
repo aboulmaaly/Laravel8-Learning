b3cdb9ec0f2578503999d5096e4b8f1b3a43b714
Mohamed Aboulmaaly <maboulmaaly@yahoo.com>
2021-05-17 18:51:28 +0100

    Fix laravel Mix run dev error ...

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\CarsController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', [PagesController::class, 'index']);
 Route::get('/about', [PagesController::class, 'about']);
@@ -11,3 +12,12 @@ Route::get('/about', [PagesController::class, 'about']);
 Route::get('/posts', [PostsController::class, 'index']);
 
 Route::resource('/cars', CarsController::class);
+
+Auth::routes();
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
